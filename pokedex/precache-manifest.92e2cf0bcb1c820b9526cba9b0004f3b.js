9837b2b5390ecde9240cc64b1ff003cea7b1d579
Paweł Fulara <fulara.pawel@gmail.com>
2020-02-03 20:52:49 +0100

    upload pokedex

---
+++ pokedex/precache-manifest.92e2cf0bcb1c820b9526cba9b0004f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc32bcc6cdbb63d8582437a811520e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b43cf16d265745cf9c85",
+    "url": "/static/css/main.4fd53481.chunk.css"
+  },
+  {
+    "revision": "b7eefa64ffc4010f72ba",
+    "url": "/static/js/2.7570b8da.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7570b8da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43cf16d265745cf9c85",
+    "url": "/static/js/main.9e6c0b10.chunk.js"
+  },
+  {
+    "revision": "a3ae24c73f7b90477b95",
+    "url": "/static/js/runtime-main.801dd740.js"
+  }
+]);

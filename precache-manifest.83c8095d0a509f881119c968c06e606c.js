99086ec1cd89c4468e60f0b51ec1cc9f30bbf576
Paweł Fulara <fulara.pawel@gmail.com>
2020-03-22 20:53:20 +0100

    update portfolio

---
+++ precache-manifest.83c8095d0a509f881119c968c06e606c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2437f1d0ffa1e7290a13dbd2236f1489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48e7998a11ed6feb448",
+    "url": "/static/css/main.98dbb99a.chunk.css"
+  },
+  {
+    "revision": "fca7a28fd25111e1eabe",
+    "url": "/static/js/2.5fb2f14d.chunk.js"
+  },
+  {
+    "revision": "b48e7998a11ed6feb448",
+    "url": "/static/js/main.773db3ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

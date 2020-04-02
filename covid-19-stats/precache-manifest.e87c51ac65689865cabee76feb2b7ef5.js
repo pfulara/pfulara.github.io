4fe36136ae04395d6d8740b86fdd6a2917e5d889
Paweł <fulara.pawel@gmail.com>
2020-04-02 10:24:13 +0200

    upload covid-19 page

---
+++ covid-19-stats/precache-manifest.e87c51ac65689865cabee76feb2b7ef5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c801197a4a535475663496c4b6a6cab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78de4d1fbf3eda1adc55",
+    "url": "/static/css/main.b1778c73.chunk.css"
+  },
+  {
+    "revision": "f4efeac985b7bef10ab6",
+    "url": "/static/js/2.3b118b55.chunk.js"
+  },
+  {
+    "revision": "78de4d1fbf3eda1adc55",
+    "url": "/static/js/main.7c512fc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

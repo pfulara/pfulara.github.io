35821e32735406da81eab363d9c2ced98f62f69f
pfulara <fulara.pawel@gmail.com>
2024-05-15 14:32:59 +0200

    Portfolio update

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    crossOrigin: 'anonymous',
-    assetPrefix: '/',
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'firebasestorage.googleapis.com',
-            port: '',
-            pathname: '/v0/b/portfolio-44cf3.appspot.com/**',
-          },
-        ],
+  reactStrictMode: true,
+  output: 'export',
+  crossOrigin: 'anonymous',
+  assetPrefix: '/',
+  distDir: 'docs',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/portfolio-44cf3.appspot.com/**',
       },
+    ],
+  },
 };
 
 export default nextConfig;

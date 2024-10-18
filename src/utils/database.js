6bae539dc96688acd238ea8da8b8693ac42169e2
pfulara <fulara.pawel@gmail.com>
2024-10-18 15:27:38 +0200

    env

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,15 +1,18 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: "portfolio-44cf3.firebaseapp.com",
-    databaseURL: "https://portfolio-44cf3.firebaseio.com",
-    projectId: "portfolio-44cf3",
-    storageBucket: "portfolio-44cf3.appspot.com",
-    messagingSenderId: "180957930327",
-    appId: "1:180957930327:web:68a9dd9e0e75069ee71412"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
-let firebase = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+let firebase =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
 
 export default firebase;
